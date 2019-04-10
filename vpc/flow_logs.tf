450be2c5deff6bbdef4f4e38e906df599f13b558
iamsudip <iamsudip@programmer.net>
2019-04-10 15:03:00 +0530

    vpc: fix flow logs

--- vpc/flow_logs.tf
+++ vpc/flow_logs.tf
@@ -1,9 +1,9 @@
 resource "aws_flow_log" "all" {
-  count          = "${var.enable_flow_logs ? 1 : 0}"
-  log_group_name = "${aws_cloudwatch_log_group.flow_logs.name}"
-  iam_role_arn   = "${aws_iam_role.vpc_flow_logs.arn}"
-  vpc_id         = "${aws_vpc.default.id}"
-  traffic_type   = "ALL"
+  count           = "${var.enable_flow_logs ? 1 : 0}"
+  log_destination = "${aws_cloudwatch_log_group.flow_logs.arn}"
+  iam_role_arn    = "${aws_iam_role.vpc_flow_logs.arn}"
+  vpc_id          = "${aws_vpc.default.id}"
+  traffic_type    = "ALL"
 }
 
 resource "aws_cloudwatch_log_group" "flow_logs" {
@@ -19,7 +19,7 @@ resource "aws_cloudwatch_log_group" "flow_logs" {
 
 resource "aws_iam_role" "vpc_flow_logs" {
   count = "${var.enable_flow_logs ? 1 : 0}"
-  name  = "${local.resource_identifier}-vpc-flow-logs}"
+  name  = "${local.resource_identifier}-vpc-flow-logs"
 
   assume_role_policy = <<EOF
 {
