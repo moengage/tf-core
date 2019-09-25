63bbc4f6d83eab7f12963561700386eecfe9bb13
iamsudip <iamsudip@programmer.net>
2019-09-25 15:48:07 +0530

    state_backend: add public block for s3 bucket

--- state_backend/bucket.tf
+++ state_backend/bucket.tf
@@ -17,3 +17,10 @@ resource "aws_s3_bucket" "default" {
   )
 }
 
+resource "aws_s3_bucket_public_access_block" "default" {
+  bucket = aws_s3_bucket.default.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
