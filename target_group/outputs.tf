080bb49e50d1ae512587797e57ee3900b5df8500
Souradeep De <souradeep@moengage.com>
2019-12-05 17:08:07 +0530

    target_group: fix tg outputs

--- target_group/outputs.tf
+++ target_group/outputs.tf
@@ -1,12 +1,11 @@
 output "arn" {
-  value = aws_lb_target_group.default[0].arn
+  value = join(", ", aws_lb_target_group.default.*.arn)
 }
 
 output "arn_suffix" {
-  value = aws_lb_target_group.default[0].arn_suffix
+  value = join(", ", aws_lb_target_group.default.*.arn_suffix)
 }
 
 output "name" {
-  value = aws_lb_target_group.default[0].name
+  value = join(", ", aws_lb_target_group.default.*.name)
 }
-
