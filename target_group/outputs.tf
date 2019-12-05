804cf909776fe056e8a4750532e2f59057851e9e
Souradeep De <souradeep@moengage.com>
2019-12-05 16:36:01 +0530

    target_group: fix target group output variables

--- target_group/outputs.tf
+++ target_group/outputs.tf
@@ -1,12 +1,12 @@
 output "arn" {
-  value = aws_lb_target_group.tg.arn
+  value = aws_lb_target_group.default.arn
 }
 
 output "arn_suffix" {
-  value = aws_lb_target_group.tg.arn_suffix
+  value = aws_lb_target_group.default.arn_suffix
 }
 
 output "name" {
-  value = aws_lb_target_group.tg.name
+  value = aws_lb_target_group.default.name
 }
 
--- target_group/target_group.tf
+++ target_group/target_group.tf
@@ -36,4 +36,3 @@ resource "aws_lb_target_group" "default" {
     },
   )
 }
-
