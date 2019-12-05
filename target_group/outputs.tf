65d3faa8e21bd5ef6b3b22c1443bda6b2bb001ff
Souradeep De <souradeep@moengage.com>
2019-12-05 16:47:58 +0530

    target_group: access first element of tg output

--- target_group/outputs.tf
+++ target_group/outputs.tf
@@ -1,12 +1,12 @@
 output "arn" {
-  value = aws_lb_target_group.default.arn
+  value = aws_lb_target_group.default[0].arn
 }
 
 output "arn_suffix" {
-  value = aws_lb_target_group.default.arn_suffix
+  value = aws_lb_target_group.default[0].arn_suffix
 }
 
 output "name" {
-  value = aws_lb_target_group.default.name
+  value = aws_lb_target_group.default[0].name
 }
 
