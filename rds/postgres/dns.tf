ce7259f3ab29cca4c6c36f50ae8eed4cf0f9044b
aleem-99 <31549030+aleem-99@users.noreply.github.com>
2022-01-27 18:23:36 +0530

    RDS dns fix (#39)

---
+++ rds/postgres/dns.tf
@@ -0,0 +1,12 @@
+data "aws_route53_zone" "rds_dns_hosted_zone" {
+  name   = var.zone_name
+  vpc_id = var.vpc_id
+}
+
+resource "aws_route53_record" "rds_dns" {
+  zone_id = data.aws_route53_zone.rds_dns_hosted_zone.id
+  name    = var.rds_dns
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_db_instance.postgresql.address]
+}
