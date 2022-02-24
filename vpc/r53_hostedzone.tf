ff7cf3f330e116038ede3ebaf1632f739099b5ea
aleem-99 <aleem@moengage.com>
2022-02-24 17:09:39 +0530

    linting

--- vpc/r53_hostedzone.tf
+++ vpc/r53_hostedzone.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_zone" "private_hosted_zone" {
-  count = length(var.route53_zone_list)
-  name =  var.route53_zone_list[count.index]
+  count   = length(var.route53_zone_list)
+  name    = var.route53_zone_list[count.index]
   comment = lower(var.region)
   vpc {
     vpc_id = aws_vpc.default.id
