a5ab27a28bc4904ffb9441cacd7ae83d685355f4
Arnold Arun Dsouza <arnold.dsouza@moengage.com>
2022-03-01 12:21:51 +0530

    addition of policy and alarm creation for asg

--- autoscaling_group/policy.tf
+++ autoscaling_group/policy.tf
@@ -1,3 +1,48 @@
+locals {
+  autoscaling_enabled = var.autoscaling_policies_enabled
+  asg_name            = join("", aws_autoscaling_group.default.*.name)
+
+  default_ec2_alarms = {
+    cpu_high = {
+      alarm_name                = "${local.asgname}cpuhigh"
+      comparison_operator       = "GreaterThanOrEqualToThreshold"
+      evaluation_periods        = var.cpu_utilization_high_evaluation_periods
+      metric_name               = "CPUUtilization"
+      namespace                 = "AWS/EC2"
+      period                    = var.cpu_utilization_high_period_seconds
+      statistic                 = var.cpu_utilization_high_statistic
+      threshold                 = var.cpu_utilization_high_threshold_percent
+      dimensions_name           = "AutoScalingGroupName"
+      dimensions_target         = join("", aws_autoscaling_group.default.*.name)
+      alarm_description         = "Scale up if CPU utilization is above ${var.cpu_utilization_high_threshold_percent} for ${var.cpu_utilization_high_period_seconds} * {var.cpu_utilization_high_evaluation_periods} seconds"
+      alarm_actions             = [join("", aws_autoscaling_policy.scale_up.*.arn)]
+      treat_missing_data        = "missing"
+      ok_actions                = [var.sns_topic_alarms_arn]
+      insufficient_data_actions = [var.sns_topic_alarms_arn]
+    },
+    cpu_low = {
+      alarm_name                = "${local.asgname}cpulow"
+      comparison_operator       = "LessThanOrEqualToThreshold"
+      evaluation_periods        = var.cpu_utilization_low_evaluation_periods
+      metric_name               = "CPUUtilization"
+      namespace                 = "AWS/EC2"
+      period                    = var.cpu_utilization_low_period_seconds
+      statistic                 = var.cpu_utilization_low_statistic
+      threshold                 = var.cpu_utilization_low_threshold_percent
+      dimensions_name           = "AutoScalingGroupName"
+      dimensions_target         = join("", aws_autoscaling_group.default.*.name)
+      alarm_description         = "Scale down if the CPU utilization is below ${var.cpu_utilization_low_threshold_percent} for ${var.cpu_utilization_low_period_seconds} * {var.cpu_utilization_high_evaluation_periods} seconds"
+      alarm_actions             = [join("", aws_autoscaling_policy.scale_down.*.arn)]
+      treat_missing_data        = "missing"
+      ok_actions                = [var.sns_topic_alarms_arn]
+      insufficient_data_actions = [var.sns_topic_alarms_arn]
+    }
+  }
+
+  default_alarms = var.autoscaling_policies_enabled && var.default_alarms_enabled ? local.default_ec2_alarms : {}
+  all_alarms     = var.autoscaling_policies_enabled ? merge(local.default_alarms, var.custom_alarms) : {}
+}
+
 resource "aws_autoscaling_schedule" "schedulers" {
   for_each = var.create_schedule ? var.schedules : {}
 
@@ -64,7 +109,7 @@ resource "aws_autoscaling_policy" "targetandpredictive" {
 
 resource "aws_autoscaling_policy" "scale_up" {
   count                  = local.autoscaling_enabled ? 1 : 0
-  name                   = "scaleup"
+  name                   = "${local.asgname}scaleup"
   autoscaling_group_name = join("", aws_autoscaling_group.default.*.name)
   adjustment_type        = var.scale_up_adjustment_type
   policy_type            = var.scale_up_policy_type
@@ -81,56 +126,12 @@ resource "aws_autoscaling_policy" "scale_up" {
 
 resource "aws_autoscaling_policy" "scale_down" {
   count                  = local.autoscaling_enabled ? 1 : 0
-  name                   = "scaledown"
+  name                   = "${local.asgname}scaledown"
   scaling_adjustment     = var.scale_down_scaling_adjustment
   adjustment_type        = var.scale_down_adjustment_type
   policy_type            = var.scale_down_policy_type
   cooldown               = var.scale_down_cooldown_seconds
   autoscaling_group_name = join("", aws_autoscaling_group.default.*.name)
-}
-
-locals {
-  autoscaling_enabled = var.autoscaling_policies_enabled
-
-  default_ec2_alarms = {
-    cpu_high = {
-      alarm_name                = "cpuhigh"
-      comparison_operator       = "GreaterThanOrEqualToThreshold"
-      evaluation_periods        = var.cpu_utilization_high_evaluation_periods
-      metric_name               = "CPUUtilization"
-      namespace                 = "AWS/EC2"
-      period                    = var.cpu_utilization_high_period_seconds
-      statistic                 = var.cpu_utilization_high_statistic
-      threshold                 = var.cpu_utilization_high_threshold_percent
-      dimensions_name           = "AutoScalingGroupName"
-      dimensions_target         = join("", aws_autoscaling_group.default.*.name)
-      alarm_description         = "Scale up if CPU utilization is above ${var.cpu_utilization_high_threshold_percent} for ${var.cpu_utilization_high_period_seconds} * {var.cpu_utilization_high_evaluation_periods} seconds"
-      alarm_actions             = [join("", aws_autoscaling_policy.scale_up.*.arn)]
-      treat_missing_data        = "missing"
-      ok_actions                = [var.sns_topic_alarms_arn]
-      insufficient_data_actions = [var.sns_topic_alarms_arn]
-    },
-    cpu_low = {
-      alarm_name                = "cpulow"
-      comparison_operator       = "LessThanOrEqualToThreshold"
-      evaluation_periods        = var.cpu_utilization_low_evaluation_periods
-      metric_name               = "CPUUtilization"
-      namespace                 = "AWS/EC2"
-      period                    = var.cpu_utilization_low_period_seconds
-      statistic                 = var.cpu_utilization_low_statistic
-      threshold                 = var.cpu_utilization_low_threshold_percent
-      dimensions_name           = "AutoScalingGroupName"
-      dimensions_target         = join("", aws_autoscaling_group.default.*.name)
-      alarm_description         = "Scale down if the CPU utilization is below ${var.cpu_utilization_low_threshold_percent} for ${var.cpu_utilization_low_period_seconds} * {var.cpu_utilization_high_evaluation_periods} seconds"
-      alarm_actions             = [join("", aws_autoscaling_policy.scale_down.*.arn)]
-      treat_missing_data        = "missing"
-      ok_actions                = [var.sns_topic_alarms_arn]
-      insufficient_data_actions = [var.sns_topic_alarms_arn]
-    }
-  }
-
-  default_alarms = var.autoscaling_policies_enabled && var.default_alarms_enabled ? local.default_ec2_alarms : {}
-  all_alarms     = var.autoscaling_policies_enabled ? merge(local.default_alarms, var.custom_alarms) : {}
 }
 
 resource "aws_cloudwatch_metric_alarm" "all_alarms" {
