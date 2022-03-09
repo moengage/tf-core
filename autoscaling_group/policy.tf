cfe499c6cb6e23d01d27d62d192584c6508c4f59
Arnold Arun Dsouza <arnold.dsouza@moengage.com>
2022-03-10 00:01:52 +0530

    addition of policy and alarm creation for asg

--- autoscaling_group/policy.tf
+++ autoscaling_group/policy.tf
@@ -122,9 +122,9 @@ resource "aws_autoscaling_policy" "scale_up" {
   dynamic "step_adjustment" {
     for_each = toset(lookup(each.value, "step_adjustment", null) != null ? [each.value.step_adjustment] : [])
     content {
-      scaling_adjustment          = step_adjustment.value[*].scaling_adjustment
-      metric_interval_lower_bound = step_adjustment.value[*].metric_interval_lower_bound
-      metric_interval_upper_bound = step_adjustment.value[*].metric_interval_upper_bound
+      scaling_adjustment          = step_adjustment.value.scaling_adjustment
+      metric_interval_lower_bound = step_adjustment.value.metric_interval_lower_bound
+      metric_interval_upper_bound = step_adjustment.value.metric_interval_upper_bound
     }
   }
 }
