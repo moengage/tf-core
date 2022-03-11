f7462ac05662b73fbd1127ab7e1ae7a6fb075fcc
Arnold Arun Dsouza <arnold.dsouza@moengage.com>
2022-03-11 13:33:15 +0530

    addition of policy and alarm creation for asg

--- autoscaling_group/outputs.tf
+++ autoscaling_group/outputs.tf
@@ -9,3 +9,15 @@ output "id" {
 output "name" {
   value = join(",", aws_autoscaling_group.default.*.name)
 }
+
+output "autoscaling_policy_scaledown_arns" {
+  value = values(aws_autoscaling_policy.scale_down)[*].arn
+}
+
+output "autoscaling_policy_scaleup_arns" {
+  value = values(aws_autoscaling_policy.scale_up)[*].arn
+}
+
+output "alarm_arns" {
+  value = { for k, v in aws_cloudwatch_metric_alarm.all_alarms : k => v }
+}
