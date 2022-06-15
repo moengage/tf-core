f728821b2be26c10c1fe7d40ce12d83c4710ea17
kartik-sareen <kartik.sareen@moengage.com>
2022-06-15 11:26:55 +0530

    rds-iam-role

--- rds/postgres/main.tf
+++ rds/postgres/main.tf
@@ -1,28 +1,4 @@
-# IAM resources
-data "aws_iam_policy_document" "enhanced_monitoring" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["monitoring.rds.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
 data "aws_caller_identity" "current" {}
-
-resource "aws_iam_role" "enhanced_monitoring" {
-  name               = "rds-${local.resource_identifier}-EnhancedMonitoringIAMRole"
-  assume_role_policy = data.aws_iam_policy_document.enhanced_monitoring.json
-}
-
-resource "aws_iam_role_policy_attachment" "enhanced_monitoring" {
-  role       = aws_iam_role.enhanced_monitoring.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
-}
 
 resource "aws_db_subnet_group" "default" {
   name        = local.resource_identifier
