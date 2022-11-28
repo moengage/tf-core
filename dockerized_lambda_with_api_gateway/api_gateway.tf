39dbed0e98d5f0d2257710a2710eaca274f73390
anshit-sre-sre <105732948+anshit-sre-sre@users.noreply.github.com>
2022-11-28 23:39:23 +0530

    Update api_gateway.tf

--- dockerized_lambda_with_api_gateway/api_gateway.tf
+++ dockerized_lambda_with_api_gateway/api_gateway.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_usage_plan" "apigw_usage_plan" {
 
   api_stages {
     api_id = aws_api_gateway_rest_api.lambda_api.id
-    stage  =  aws_api_gateway_stage.prod_stage.stage_name
+    stage  =  aws_api_gateway_stage.stage.stage_name
   }
 }
 
