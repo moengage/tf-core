bf9a862f47b81d1f25828eab5b529ae9ccae747e
anshit-sre-sre <105732948+anshit-sre-sre@users.noreply.github.com>
2022-11-29 20:25:36 +0530

    Update api_gateway.tf

--- dockerized_lambda_with_api_gateway/api_gateway.tf
+++ dockerized_lambda_with_api_gateway/api_gateway.tf
@@ -79,7 +79,7 @@ resource "aws_api_gateway_method_response" "gateway_method_response" {
 }
 
 resource "aws_api_gateway_deployment" "api_gateway_deployment_get" {
-  depends_on = [aws_api_gateway_integration.lambda_integration_get,  aws_api_gateway_method.take_screenshot_get, aws_api_gateway_integration.lambda_integration_get]
+  depends_on = [aws_api_gateway_integration.lambda_integration_get,  aws_api_gateway_method.take_screenshot_get, aws_api_gateway_integration.lambda_integration_get, aws_api_gateway_method_response.gateway_method_response]
 
   rest_api_id = aws_api_gateway_rest_api.lambda_api.id
 }
