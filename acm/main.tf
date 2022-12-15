3e47b14e1bdb3bd6734deca07255f0dfd1a8bfa1
Manoj H N <manoj@moengage.com>
2022-12-15 14:20:01 +0530

    remove provider

--- acm/main.tf
+++ acm/main.tf
@@ -1,5 +1,4 @@
 resource "aws_acm_certificate" "main" {
-  provider = "aws.acm"
   domain_name = "${var.domain_names[0]}"
   subject_alternative_names = "${slice(var.domain_names, 1, length(var.domain_names))}"
   validation_method = "DNS"
@@ -10,7 +9,6 @@ resource "aws_acm_certificate" "main" {
 }
 
 resource "aws_route53_record" "validation" {
-  provider = "aws.route53"
   count = "${length(var.domain_names)}"
   name = "${lookup(aws_acm_certificate.main.domain_validation_options[count.index], "resource_record_name")}"
   type = "${lookup(aws_acm_certificate.main.domain_validation_options[count.index], "resource_record_type")}"
@@ -20,7 +18,6 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "main" {
-  provider = "aws.acm"
   certificate_arn = "${aws_acm_certificate.main.arn}"
   validation_record_fqdns = ["${aws_route53_record.validation.*.fqdn}"]
 }
