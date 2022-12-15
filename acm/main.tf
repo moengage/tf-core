9737468bb0ac075ed9cd8b4b838f04a0e1aa0263
Manoj H N <manoj@moengage.com>
2022-12-15 16:51:24 +0530

    fidx variable

--- acm/main.tf
+++ acm/main.tf
@@ -27,5 +27,5 @@ resource "aws_route53_record" "validation" {
 
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
