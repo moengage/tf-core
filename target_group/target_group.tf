dbf0102ca2a0c60ca2bb2aba8945fd9a9d3b14a0
Arnold Arun Dsouza <arnold.dsouza@moengage.com>
2022-12-27 10:53:31 +0530

    graviton support for oneami py3 infra

--- target_group/target_group.tf
+++ target_group/target_group.tf
@@ -4,7 +4,7 @@ resource "aws_lb_target_group" "default" {
     local.resource_identifier,
     0,
     min(29, length(local.resource_identifier)),
-  )}-tg"
+  )}-arm-tg"
   port                 = var.port
   protocol             = "HTTP"
   vpc_id               = var.vpc_id
