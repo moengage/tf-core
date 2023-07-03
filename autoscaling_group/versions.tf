d3f12c1ce9f656289daf072e11b66d32b727c98b
priya-sharmaa <priya.sharma@moengage.com>
2023-07-03 22:02:10 +0530

    updated versions

--- autoscaling_group/versions.tf
+++ autoscaling_group/versions.tf
@@ -1,10 +1,10 @@
 
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.5"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.76.1"
+      version = "5.6.2"
     }
   }
 }
