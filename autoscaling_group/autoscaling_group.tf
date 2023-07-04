3d2379c63681f623b8d23f9d9f47f3020d01497c
priya-sharmaa <priya.sharma@moengage.com>
2023-07-04 14:47:24 +0530

    removed the variables from 22-27

--- autoscaling_group/autoscaling_group.tf
+++ autoscaling_group/autoscaling_group.tf
@@ -19,13 +19,7 @@ resource "aws_autoscaling_group" "default" {
   wait_for_elb_capacity     = var.wait_for_elb_capacity
   protect_from_scale_in     = var.protect_from_scale_in
   capacity_rebalance        = var.capacity_rebalance
-  burstable_performance     = var.burstable_performance
-  excluded_instance_types   = var.excluded_instance_types
-  architecture_type         = var.architecture_type
-  instance_generations      = var.instance_generations
-  memory_mib                = var.memory_mib
-  vcpu_count                = var.vcpu_count
-  cpu_manufacturers         = var.cpu_manufacturers
+  
 
   dynamic "initial_lifecycle_hook" {
     for_each = var.initial_lifecycle_hooks
