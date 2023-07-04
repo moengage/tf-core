b35f4b9a082c0edc0aebe1a464f8b804ffc66171
priya-sharmaa <priya.sharma@moengage.com>
2023-07-05 00:37:29 +0530

    removed the var from  asg_with_lt folder asg file

--- autoscaling_with_launch_template/autoscaling_group.tf
+++ autoscaling_with_launch_template/autoscaling_group.tf
@@ -17,13 +17,6 @@ resource "aws_autoscaling_group" "default" {
   min_elb_capacity          = var.min_elb_capacity
   wait_for_elb_capacity     = var.wait_for_elb_capacity
   protect_from_scale_in     = var.protect_from_scale_in
-  burstable_performance     = var.burstable_performance
-  excluded_instance_types   = var.excluded_instance_types
-  architecture_type         = var.architecture_type
-  instance_generations      = var.instance_generations
-  memory_mib                = var.memory_mib
-  vcpu_count                = var.vcpu_count
-  cpu_manufacturers         = var.cpu_manufacturers
 
   mixed_instances_policy {
     launch_template {
@@ -71,9 +64,7 @@ resource "aws_autoscaling_group" "default" {
       override {
         instance_type = length(var.instance_types) >= 10 ? var.instance_types.9 : ""
       }
-      
     }
-
     instances_distribution {
       on_demand_allocation_strategy            = var.on_demand_allocation_strategy
       on_demand_base_capacity                  = var.on_demand_base_capacity
