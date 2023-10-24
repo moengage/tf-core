18d7b127f73415dc084777728fd3459db7cfed57
manoj-me <manoj@moengage.com>
2023-10-24 19:37:24 +0530

    change policy

--- load_balancers/application/alb_logs.tf
+++ load_balancers/application/alb_logs.tf
@@ -27,3 +27,54 @@ resource "aws_s3_bucket" "logs" {
   )
 }
 
+resource "aws_s3_bucket_policy" "alb_access_logs_bucket_policy" {
+  bucket = aws_s3_bucket.logs[0].id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllowELBRootAccount",
+        "Effect" : "Allow",
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::${local.logs_bucket_name}/*",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::${var.aws_lb_accounts[data.aws_region.current.name]}:root"
+        }
+      },
+      {
+        "Sid" : "AWSLogDeliveryWrite",
+        "Effect" : "Allow",
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::${local.logs_bucket_name}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "s3:x-amz-acl" : "bucket-owner-full-control"
+          }
+        },
+        "Principal" : {
+          "Service" : "delivery.logs.amazonaws.com"
+        }
+      },
+      {
+        "Sid" : "AWSLogDeliveryAclCheck",
+        "Effect" : "Allow",
+        "Action" : "s3:GetBucketAcl",
+        "Resource" : "arn:aws:s3:::${local.logs_bucket_name}",
+        "Principal" : {
+          "Service" : "delivery.logs.amazonaws.com"
+        }
+      },
+      {
+        "Sid" : "AllowALBAccess",
+        "Effect" : "Allow",
+        "Action" : "s3:PutObject",
+        "Resource" : "arn:aws:s3:::${local.logs_bucket_name}/*",
+        "Principal" : {
+          "Service" : "elasticloadbalancing.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
